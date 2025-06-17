aaf47265e030497fec0cdbb98e4b46b624082dc3
Yogesh Patil <officialyogeshp@gmail.com>
2025-06-17 01:15:29 -0400

    Implement build setup for Vercel deployment; add user input handling, essential static files, and service worker for caching.

---
+++ build/precache-manifest.d8f3398dde98e648de845da6f3476e5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef37e8efaa582f643bcb5ca53e28039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "852702322beb80b037ea",
+    "url": "/static/js/2.f2038158.chunk.js"
+  },
+  {
+    "revision": "d60fc3b78669f6fd36817b02d468d6f1",
+    "url": "/static/js/2.f2038158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2054f8aae9701f720df",
+    "url": "/static/js/main.4d4c28dc.chunk.js"
+  },
+  {
+    "revision": "8a73b3fa4209a4f8a0c9",
+    "url": "/static/js/runtime-main.396c5088.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/logo.d7265326.png"
+  }
+]);
